f1608ea3ff9e56546aba47d9988d5af3640ed03e
Rafael Aslanian <127808926+nxfi777@users.noreply.github.com>
2024-02-22 20:45:40 +0000

    url endpoint

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, request, jsonify
-from flask_httpauth import HTTPBasicAuth  # Import HTTPBasicAuth
+from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
 from magika import Magika
 import os
+import requests
 
 app = Flask(__name__)
-auth = HTTPBasicAuth()  # Create an instance of HTTPBasicAuth
+auth = HTTPBasicAuth()
 magika_instance = Magika()
 
 users = {
@@ -19,11 +20,28 @@ def verify_password(username, password):
     return False
 
 @app.route('/identify_bytes', methods=['POST'])
-@auth.login_required  # Protect the route with basic authentication
+@auth.login_required # Basic Auth
 def identify_bytes():
     content = request.data
     result = magika_instance.identify_bytes(content)
     return jsonify(result.output)
+
+@app.route('/identify_from_url', methods=['POST'])
+@auth.login_required
+def identify_from_url():
+    data = request.get_json()
+    file_url = data.get('url')
+    if not file_url:
+        return jsonify({"error": "URL is required"}), 400
+
+    try:
+        response = requests.get(file_url)
+        response.raise_for_status()  # Raises a HTTPError if the response status code is 4XX/5XX
+        content = response.content
+        result = magika_instance.identify_bytes(content)
+        return jsonify(result.output)
+    except requests.RequestException as e:
+        return jsonify({"error": str(e)}), 500
 
 @app.route('/health', methods=['GET'])
 def health_check():
